9db0dcf5bb1a24f16a5f8f761ee957917933d091
Thato Mongwe <thatomongwe47@gmail.com>
2024-05-09 11:57:32 +0200

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,27 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+require 'oniguruma'
+
+def check_for_school(text)
+  # Oniguruma regex for "School" (case-insensitive)
+  school_regex = Oniguruma::ORegex.new("School", options: Oniguruma::OPTION_IGNORECASE)
+
+  # Check if the argument matches the regex
+  match = school_regex.match(text)
+
+  if match
+    puts "The string '#{text}' contains 'School'."
+  else
+    puts "The string '#{text}' does not contain 'School'."
+  end
+end
+
+# Get the argument from the command line (if any)
+argument = ARGV.first
+
+# Check if an argument was provided
+if argument
+  check_for_school(argument)
+else
+  puts "Please provide a string as an argument."
+end
