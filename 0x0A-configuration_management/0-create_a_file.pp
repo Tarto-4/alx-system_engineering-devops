485a88316ed6f158b0cd792ae4d996d742ea750d
Thato Mongwe <thatomongwe47@gmail.com>
2024-06-29 00:27:18 +0200

    Creates a file named 'school' in /tmp with specific ownership and permissions.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file named 'school' in /tmp with specific ownership and permissions.
+
+file { '/tmp/school':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
