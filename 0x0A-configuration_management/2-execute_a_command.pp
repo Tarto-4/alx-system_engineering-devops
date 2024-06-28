8a575bbe76d1c4eae2654fb32031ebbfc03a6c0f
Thato Mongwe <thatomongwe47@gmail.com>
2024-06-29 00:47:41 +0200

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
-# Kills the process named 'killmenow'
-exec { 'killmenow':
-  command => 'pkill killmenow',
-  path    => '/usr/bin:/bin',   
+# kill a process named killmenow
+exec { 'kill_process':
+  command  => 'pkill -f killmenow',
+  provider => 'shell',
+  path     => ['/usr/bin', '/usr/sbin',]
 }
